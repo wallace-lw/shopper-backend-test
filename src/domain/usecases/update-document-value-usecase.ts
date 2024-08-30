13dfe8fb11ba3bb19765b55f2a6415e49cfbc70e
wallace-lw <wallacelw22@gmail.com>
2024-08-30 18:01:40 -0300

    feat: add update document value use case

---
+++ src/domain/usecases/update-document-value-usecase.ts
@@ -0,0 +1,30 @@
+import { AppError } from "@/interface/helpers/errors";
+import { Errors } from "@/interface/shared/Errors";
+import { DocumentRepository } from "../repository/document-repository";
+
+export type UpdateInputDTO = {
+	confirmedValue: number;
+	measureUuid: string;
+};
+
+export class UpdateDocumentValueUseCase {
+	constructor(private readonly documentRepository: DocumentRepository) {}
+
+	async execute(dto: UpdateInputDTO) {
+		const response = await this.documentRepository.findById(dto.measureUuid);
+		if (!response) {
+			throw new AppError(Errors.MEASURE_NOT_FOUND, 404);
+		}
+		if (response.has_confirmed) {
+			throw new AppError(Errors.CONFIRMATION_DUPLICATE, 409);
+		}
+		const output = await this.documentRepository.update(
+			{
+				measureUuid: dto.measureUuid,
+			},
+			dto.confirmedValue,
+		);
+
+		return output;
+	}
+}
