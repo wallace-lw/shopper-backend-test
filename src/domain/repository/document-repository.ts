1885363f1614d879d9bfd7e71f2bdc9894f0226d
wallace-lw <wallacelw22@gmail.com>
2024-08-30 18:00:13 -0300

    feat: add repository contract interface for the implementation of the sqlite repository

--- src/domain/repository/document-repository.ts
+++ src/domain/repository/document-repository.ts
@@ -1,6 +1,15 @@
+import { Document } from "../entity";
+import { MeasureType } from "../utils";
+
 export interface DocumentRepository {
-	create(document: DocumentType): Promise<Document>;
-	update(id: number, document: Partial<Document>): Promise<boolean>;
-	find(id: number): Promise<Document>;
-	findAll(customerCode: number): Promise<Document[]>;
+	create(document: Document): Promise<Document>;
+	find(
+		customerCode: string,
+		type: MeasureType,
+		datetime: number,
+	): Promise<boolean>;
+	update(document: Partial<Document>, confirmedValue: number): Promise<boolean>;
+	// biome-ignore lint/suspicious/noExplicitAny: <>
+	findById(id: string): Promise<any>;
+	findAll(customerCode: string, measureType?: MeasureType): Promise<Document[]>;
 }
