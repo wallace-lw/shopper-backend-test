7336c985bb3fee72f0b00b89d48c404dcd9ae6f1
wallace-lw <wallacelw22@gmail.com>
2024-08-30 18:03:42 -0300

    feat: add the find all documents measures controller

---
+++ src/interface/controller/get-all-documents-measures-controller.ts
@@ -0,0 +1,34 @@
+import {
+	ListAllDocumentsUseCase,
+	ListAllInputDTO,
+} from "@/domain/usecases/get-all-documents-measures-usecases";
+import { MeasureType } from "@/domain/utils";
+import { AppError } from "../helpers/errors";
+import { Errors } from "../shared/Errors";
+
+export class ListAllDocumentsController {
+	constructor(
+		private readonly listAllDocumentsUseCase: ListAllDocumentsUseCase,
+	) {}
+
+	async execute(dto: ListAllInputDTO) {
+		if (!dto.customerCode) {
+			throw new AppError(Errors.CUSTOMER_CODE_REQUIRED, 400);
+		}
+
+		if (
+			dto.type &&
+			!Object.values(MeasureType).includes(dto.type as MeasureType)
+		) {
+			throw new AppError(Errors.INVALID_TYPE, 400);
+		}
+
+		const response = await this.listAllDocumentsUseCase.execute(dto);
+
+		return response;
+	}
+
+	private isEnumValuePresent(obj: string, enumType: MeasureType) {
+		return Object.values(enumType).includes(obj);
+	}
+}
