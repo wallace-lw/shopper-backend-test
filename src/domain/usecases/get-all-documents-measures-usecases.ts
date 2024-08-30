12c1987e760a73684d6bed2cb3392b238fc32518
wallace-lw <wallacelw22@gmail.com>
2024-08-30 18:00:56 -0300

    feat: add the list all measures use case

---
+++ src/domain/usecases/get-all-documents-measures-usecases.ts
@@ -0,0 +1,31 @@
+import { AppError } from "@/interface/helpers/errors";
+import { Errors } from "@/interface/shared/Errors";
+import { DocumentRepository } from "../repository";
+import { MeasureType } from "../utils";
+
+export type ListAllInputDTO = {
+	customerCode: string;
+	type?: MeasureType | null;
+};
+
+export class ListAllDocumentsUseCase {
+	constructor(private readonly documentRepository: DocumentRepository) {}
+
+	async execute(dto: ListAllInputDTO) {
+		const response = await this.documentRepository.findAll(
+			dto.customerCode,
+			dto.type as MeasureType,
+		);
+
+		if (!response) {
+			throw new AppError(Errors.MEASURES_NOT_FOUND, 404);
+		}
+
+		const output = {
+			customer_code: dto.customerCode,
+			measures: response,
+		};
+
+		return output;
+	}
+}
