5e8d73a6812950ea47fdbbdc2e0f1b6dfedb5f01
wallace-lw <wallacelw22@gmail.com>
2024-08-30 17:54:34 -0300

    refactor: renaming files for better pattern

--- src/application/service/extract-image-value.ts
+++
@@ -1,6 +0,0 @@
-export class ExtractImageValue {
-	async execute() {
-		// Implementation here
-		return "image_value"; // Placeholder value
-	}
-}
--- src/framework/route/app-router.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-
-export interface AppRouter {
-	path: string;
-	router: Router;
-}
